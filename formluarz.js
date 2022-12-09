ae123d3a5b12718829e278621f86664582463525
grzegorz szczepkowski <89992153+yrzer@users.noreply.github.com>
2022-12-09 11:17:48 +0100

    Add files via upload

---
+++ formluarz.js
@@ -0,0 +1,14 @@
+const run = function () {
+    var imie = document.getElementById("imie").value;
+    var nazwisko = document.getElementById("nazwisko").value;
+    var mail = document.getElementById("mail").value;
+    var kod = document.getElementById("kod").value;
+    var ul = document.getElementById("ul").value;
+    var ul_nr = document.getElementById("ul_nr").value;
+    var id = document.getElementById("id").value; // więcej może być
+    var ile = document.getElementById("ile").value;
+    var regulamin = document.getElementById("regulamin").value;
+    var error = document.getElementById("error");
+
+
+}
